6c20cc3b45c7bb44d513eb82ad9fcf868d037612
Anıl Kuşçu <63120231+anilkusc@users.noreply.github.com>
2020-06-18 13:21:02 +0300

    Update image-updater.sh

--- image-updater.sh
+++ image-updater.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-##Specify and pull images
-#images=( nginx alpine busybox )
-##INclude and exclude images
-#images=( $(docker images | grep myimage | grep -v dev | grep -v staging | grep -v none | awk '{print $1}' ) )
-##update all images
-images=( $(docker images | awk '{print $1}' ) )
+repository_name=$1
+tag=$2
+images=($(docker images | grep $repository_name | grep $tag | awk '{print $1}'))
+
 for image in "${images[@]}"
 do
-docker pull $image
+   docker pull $image:$tag
 done
